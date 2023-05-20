5f6eacd72c73e1aab3b9d5c38516cb4904e429cd
fernandopaso <fepaso@hotmail.com>
2023-05-20 11:54:09 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,8 @@
+let base1=5;
+let altura1=3;
+let base2=12.5;
+let altura2=5.3;
+let area1=base1*altura1;
+let area2=base2*altura2;
+console.log(area1);
+console.log(area2);
---
+++ practico0/promedio3.js
@@ -0,0 +1,15 @@
+let num1=5;
+let num2=7;
+let num3=9;
+let num4=-2;
+let num5=-3;
+let num6=-8;
+let num7=-1.2;
+let num8=5.3;
+let num9=-3.3;
+prom1=(num1+num2+num3)/3;
+prom2=(num4+num5+num6)/3;
+prom3=(num7+num8+num9)/3;
+console.log(prom1);
+console.log(prom2);
+console.log(prom3);
