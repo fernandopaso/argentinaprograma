f173175cd980ed1ee98e0a4dcc3def42fb1cba27
fernandopaso <fepaso@hotmail.com>
2023-06-21 16:18:58 -0300

    listaDeInvitados con extra

--- practico3/listaDeInvitados.js
+++ practico3/listaDeInvitados.js
@@ -9,13 +9,28 @@ for (let i=0;i<listaDePersonas.length;i++)
             }
         else {admitidos.push(listaDePersonas[i]);}
     }   
-console.log("La lista de invitados admitidos es:")
+console.log("La lista de invitados admitidos es:");
 for (i=0;i<admitidos.length;i++)
     {
         console.log("%s",admitidos[i]);
     }
-console.log("La lista de invitados rechazados es:")
+console.log("");
+console.log("La lista de invitados rechazados es:");
 for (i=0;i<rechazados.length;i++)
     {
         console.log("%s",rechazados[i]);
+    }
+let rechazadosOrdenados=rechazados.sort();
+let admitidosOrdenados=admitidos.sort();
+console.log("");
+console.log("La lista ordenada de invitados admitidos es:")
+for (i=0;i<admitidosOrdenados.length;i++)
+    {
+        console.log("%s",admitidosOrdenados[i]);
+    }
+console.log("");
+    console.log("La lista ordenada de invitados rechazados es:")
+for (i=0;i<rechazadosOrdenados.length;i++)
+    {
+        console.log("%s",rechazadosOrdenados[i]);
     }
